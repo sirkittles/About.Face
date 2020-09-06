9b4d474b622144498b7fc5c4c10d9b424c3519eb
Diane So <your_email_here>
2020-09-06 12:44:38 -0400

    linked navbar and added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import "./App.css";
@@ -7,11 +8,13 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <Header />
+        <Link to="/" className="title-header">
+          <Header />
+        </Link>
       </div>
-      <div className="navbar">
+      <nav>
         <NavBar />
-      </div>
+      </nav>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <h4>Navbar</h4>
+    <div className="navbar">
+      <Link to="/routine-logs">
+        <h5>Routine Logs</h5>
+      </Link>
+      <Link to="/products">
+        <h5>Products</h5>
+      </Link>
+      <Link to="/routines">
+        <h5>Routines</h5>
+      </Link>
+    </div>
   )
 }
 
