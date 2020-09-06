2b4b7bc375f69c658126ab52f45d9b922aa5e418
Diane So <your_email_here>
2020-09-06 12:04:06 -0400

    header and navbar added

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <h4>About.Face</h4>
+  )
+}
+
+export default Header;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <h4>Navbar</h4>
+  )
+}
+
+export default NavBar;
