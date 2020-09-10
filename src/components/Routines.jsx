2f168f8228bef89de3f291509b5e970b9b353c82
Diane So <your_email_here>
2020-09-09 23:53:55 -0400

    slowly getting routine to render

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -5,13 +5,21 @@ const Routines = (props) => {
   const products = props.products;
   const routines = props.routines;
   console.log(routines);
+  // const { firstCleanser, secondCleanser, toner, essence, serum, moisturizer, faceOil, sunscreen } = routines.fields;
+
   const productNames = products.map((product) => <p key={product.id}>{product.fields.name}</p>);
-
-
 
   return (
     <div className="routines-list-container">
-
+      {routines.map((routine, i) => (
+        <div key={i} className="routine">
+          <h4>{routine.fields.name}</h4>
+          {(routine.fields.firstCleanser) ? 
+            <p>First Cleanser: {routine.fields.firstCleanser}</p>
+            : ""
+          }
+        </div>
+      ))}
     </div>
   )
 }
