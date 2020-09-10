c35dc0a82be6a949eb3416a3c6951ced9034ccde
Diane So <your_email_here>
2020-09-10 00:01:06 -0400

    all routine fields rendering

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -4,7 +4,7 @@ const Routines = (props) => {
   console.log(props);
   const products = props.products;
   const routines = props.routines;
-  console.log(routines);
+  // console.log(routines);
   // const { firstCleanser, secondCleanser, toner, essence, serum, moisturizer, faceOil, sunscreen } = routines.fields;
 
   const productNames = products.map((product) => <p key={product.id}>{product.fields.name}</p>);
@@ -14,8 +14,36 @@ const Routines = (props) => {
       {routines.map((routine, i) => (
         <div key={i} className="routine">
           <h4>{routine.fields.name}</h4>
-          {(routine.fields.firstCleanser) ? 
+          {(routine.fields.firstCleanser) ?
             <p>First Cleanser: {routine.fields.firstCleanser}</p>
+            : ""
+          }
+          {(routine.fields.secondCleanser) ?
+            <p>Second Cleanser: {routine.fields.secondCleanser}</p>
+            : ""
+          }
+          {(routine.fields.toner) ?
+            <p>Toner: {routine.fields.toner}</p>
+            : ""
+          }
+          {(routine.fields.essence) ?
+            <p>Essence: {routine.fields.essence}</p>
+            : ""
+          }
+          {(routine.fields.serum) ?
+            <p>Serum: {routine.fields.serum}</p>
+            : ""
+          }
+          {(routine.fields.moisturizer) ?
+            <p>Moisturizer: {routine.fields.moisturizer}</p>
+            : ""
+          }
+          {(routine.fields.faceOil) ?
+            <p>Face Oil: {routine.fields.faceOil}</p>
+            : ""
+          }
+          {(routine.fields.sunscreen) ?
+            <p>Sunscreen: {routine.fields.sunscreen}</p>
             : ""
           }
         </div>
